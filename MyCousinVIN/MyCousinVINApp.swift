c15bd992918f3bf1d5be01b939b4822d591078cf
Philip Keller <philipjoelkeller@gmail.com>
2025-02-01 10:05:48 -0800

    Initial Commit

---
+++ MyCousinVIN/MyCousinVINApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCousinVINApp.swift
+//  MyCousinVIN
+//
+//  Created by Philip Keller on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCousinVINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
